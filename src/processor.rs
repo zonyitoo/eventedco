76c68b429e0ef7d1cc6dadda044ecb260f7e9770
Y. T. Chung <zonyitoo@gmail.com>
2015-06-14 14:16:46 +0800

    support linux

--- src/processor.rs
+++ src/processor.rs
@@ -27,6 +27,22 @@ impl Processor {
         })
     }
 
+    #[cfg(any(target_os = "linux",
+          target_os = "android"))]
+    pub fn register<E: Evented + ::std::os::unix::io::AsRawFd>(&mut self, io: &E, inst: Interest) -> io::Result<()> {
+        let cur_hdl = Coroutine::current().clone();
+        let token = self.handler.slabs.insert((io.as_raw_fd(), cur_hdl)).unwrap();
+        try!(self.event_loop.register_opt(io, token, inst, PollOpt::oneshot()));
+        Coroutine::block();
+        Ok(())
+    }
+
+    #[cfg(any(target_os = "macos",
+          target_os = "freebsd",
+          target_os = "dragonfly",
+          target_os = "ios",
+          target_os = "bitrig",
+          target_os = "openbsd"))]
     pub fn register<E: Evented>(&mut self, io: &E, inst: Interest) -> io::Result<()> {
         let cur_hdl = Coroutine::current().clone();
         let token = self.handler.slabs.insert(cur_hdl).unwrap();
@@ -40,6 +56,12 @@ impl Processor {
     }
 }
 
+#[cfg(any(target_os = "macos",
+          target_os = "freebsd",
+          target_os = "dragonfly",
+          target_os = "ios",
+          target_os = "bitrig",
+          target_os = "openbsd"))]
 struct IoHandler {
     slabs: Slab<Handle>,
 }
@@ -52,6 +74,12 @@ impl IoHandler {
     }
 }
 
+#[cfg(any(target_os = "macos",
+          target_os = "freebsd",
+          target_os = "dragonfly",
+          target_os = "ios",
+          target_os = "bitrig",
+          target_os = "openbsd"))]
 impl Handler for IoHandler {
     type Timeout = ();
     type Message = ();
@@ -69,6 +97,47 @@ impl Handler for IoHandler {
     fn writable(&mut self, _: &mut EventLoop<IoHandler>, token: Token) {
         match self.slabs.remove(token) {
             Some(hdl) => {
+                let res = hdl.resume();
+                debug!("Readable: Resume resule: {:?}", res);
+            },
+            None => {}
+        }
+    }
+}
+
+#[cfg(any(target_os = "linux",
+          target_os = "android"))]
+struct IoHandler {
+    slabs: Slab<(::std::os::unix::io::RawFd, Handle)>,
+}
+
+#[cfg(any(target_os = "linux",
+          target_os = "android"))]
+impl Handler for IoHandler {
+    type Timeout = ();
+    type Message = ();
+
+    fn readable(&mut self, event_loop: &mut EventLoop<IoHandler>, token: Token, _: ReadHint) {
+        match self.slabs.remove(token) {
+            Some((fd, hdl)) => {
+                let io: Io = ::std::convert::From::from(fd);
+                event_loop.deregister(&io);
+                ::std::mem::forget(io);
+
+                let res = hdl.resume();
+                debug!("Readable: Resume resule: {:?}", res);
+            },
+            None => {}
+        }
+    }
+
+    fn writable(&mut self, _: &mut EventLoop<IoHandler>, token: Token) {
+        match self.slabs.remove(token) {
+            Some((fd, hdl)) => {
+                let io: Io = ::std::convert::From::from(fd);
+                event_loop.deregister(&io);
+                ::std::mem::forget(io);
+
                 let res = hdl.resume();
                 debug!("Readable: Resume resule: {:?}", res);
             },
